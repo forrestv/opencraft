ceff2fba26d9e1d8756620fcb71896c196c4e2db
forrest <forrest@470744a7-cac9-478e-843e-5ec1b25c69e8>
2008-10-25 21:00:53 +0000

    game
    git-svn-id: svn://127.0.0.1:1234@251 470744a7-cac9-478e-843e-5ec1b25c69e8

---
+++ opencraft-v2/src/game/UI.py
@@ -0,0 +1,8 @@
+class UI(object):
+    def update(self, gamestate):
+        self
+    def draw(self, surface, gamestate):
+        self.camera.draw(surface)
+        if self.selection:
+            pygame.draw.rect(view.screen, (16,252,24), self.selection, 1)
+        surface.blit(read.
